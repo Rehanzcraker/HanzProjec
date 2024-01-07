e23e985df842796b672f5844907ef8120a407d38
Rehanzcraker <155465137+Rehanzcraker@users.noreply.github.com>
2024-01-07 19:54:23 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://github.com/Rehanzcraker/HanzProjec/edit/main/limit/menu.zip
+    wget https://raw.githubusercontent.com/Rehanzcraker/HanzProjec/main/limit/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
